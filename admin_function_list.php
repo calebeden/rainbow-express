43b0366cd16fbd2f738ec158aba8789ea6b7c19d
calebeden <calebeden@github.com>
2022-02-22 15:26:46 -0600

    Change delete account to generate report

--- admin_function_list.php
+++ admin_function_list.php
@@ -8,8 +8,7 @@
 		<a type="button" class="btn btn-primary mb-3 btn-lg" href="admin/edit_permissions.php" role="button">Edit Account Permissions</a>
 	</div>
 	<div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6">
-		<!-- <a type="button" class="btn btn-primary mb-3 btn-lg" href="" role="button">Delete Account</a> -->
-		<a type="button" class="btn btn-secondary mb-3 btn-lg" href="" role="button">Delete Account</a>
+		<a type="button" class="btn btn-secondary mb-3 btn-lg" href="" role="button">Generate Report</a>
 
 	</div>
 	
