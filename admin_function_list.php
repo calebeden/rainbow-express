c03fa0498fceb731d5e21a75fead0d6e59c2bf05
calebeden <calebeden@github.com>
2022-02-22 15:03:29 -0600

    Temporary change delete account button to disabled

--- admin_function_list.php
+++ admin_function_list.php
@@ -8,7 +8,9 @@
 		<a type="button" class="btn btn-primary mb-3 btn-lg" href="admin/edit_permissions.php" role="button">Edit Account Permissions</a>
 	</div>
 	<div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6">
-		<a type="button" class="btn btn-primary mb-3 btn-lg" href="" role="button">Delete Account</a>
+		<!-- <a type="button" class="btn btn-primary mb-3 btn-lg" href="" role="button">Delete Account</a> -->
+		<a type="button" class="btn btn-secondary mb-3 btn-lg" href="" role="button">Delete Account</a>
+
 	</div>
 	
 	<div class="col-xxl col-xl col-lg"></div>
